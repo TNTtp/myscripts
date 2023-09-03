75ae8b727f8660d910f72154806b37c16e7e8df1
TNTtp <99587615+TNTtp@users.noreply.github.com>
2023-09-03 23:29:02 +0200

    Update Doll.lua

--- Doll.lua
+++ Doll.lua
@@ -2,42 +2,34 @@ local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shl
 
 hiddenfling = false
 
-if game:GetService("ReplicatedStorage"):FindFirstChild("juisdfj0i32i0eidsuf0iok") then
-	
-else
-	
-	detection = Instance.new("Decal")
-	detection.Name = "juisdfj0i32i0eidsuf0iok"
-	detection.Parent = game:GetService("ReplicatedStorage")
-	local function fling()
-		local hrp, c, vel, movel = nil, nil, nil, 0.1
-		while true do
-			game:GetService("RunService").Heartbeat:Wait()
+
+local function fling()
+	local hrp, c, vel, movel = nil, nil, nil, 0.1
+	while true do
+		game:GetService("RunService").Heartbeat:Wait()
+		if hiddenfling then
+			local lp = game.Players.LocalPlayer
+			while hiddenfling and not (c and c.Parent and hrp and hrp.Parent) do
+				game:GetService("RunService").Heartbeat:Wait()
+				c = lp.Character
+				hrp = c:FindFirstChild("HumanoidRootPart") or c:FindFirstChild("Torso") or c:FindFirstChild("UpperTorso")
+			end
 			if hiddenfling then
-				local lp = game.Players.LocalPlayer
-				while hiddenfling and not (c and c.Parent and hrp and hrp.Parent) do
-					game:GetService("RunService").Heartbeat:Wait()
-					c = lp.Character
-					hrp = c:FindFirstChild("HumanoidRootPart") or c:FindFirstChild("Torso") or c:FindFirstChild("UpperTorso")
-				end
-				if hiddenfling then
-					vel = hrp.Velocity
-					hrp.Velocity = vel * 1000000 + Vector3.new(0, 1000000, 0)
-					game:GetService("RunService").RenderStepped:Wait()
-					if c and c.Parent and hrp and hrp.Parent then
+				vel = hrp.Velocity
+				hrp.Velocity = vel * 1000000 + Vector3.new(0, 1000000, 0)
+				game:GetService("RunService").RenderStepped:Wait()
+				if c and c.Parent and hrp and hrp.Parent then
 							hrp.Velocity = vel
-					end
-					game:GetService("RunService").Stepped:Wait()
-					if c and c.Parent and hrp and hrp.Parent then
-						hrp.Velocity = vel + Vector3.new(0, movel, 0)
-						movel = movel * -1
-					end
+				end
+				game:GetService("RunService").Stepped:Wait()
+				if c and c.Parent and hrp and hrp.Parent then
+					hrp.Velocity = vel + Vector3.new(0, movel, 0)
+					movel = movel * -1
 				end
 			end
 		end
 	end
-		 
-	fling()
+	wait()
 end
 
 local Window = OrionLib:MakeWindow({
@@ -63,6 +55,7 @@ Fling:AddToggle({
 	Default = false,
 	Callback = function(Value)
 		hiddenfling = true
+		fling()
 	end    
 })
 
