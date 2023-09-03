b07b69fe8790479f36a21c7faf07f7bc41b74178
TNTtp <99587615+TNTtp@users.noreply.github.com>
2023-09-03 14:51:19 +0200

    Update Doll.lua

--- Doll.lua
+++ Doll.lua
@@ -1,14 +1,38 @@
-for i,v in next, game:GetService("Players").LocalPlayer.Character:GetDescendants() do
-if v:IsA("BasePart") and v.Name ~="HumanoidRootPart" then 
-game:GetService("RunService").Heartbeat:connect(function()
-v.Velocity = Vector3.new(0,35,0)
-wait(0.5)
-end)
+hiddenfling = false
+	 if game:GetService("ReplicatedStorage"):FindFirstChild("juisdfj0i32i0eidsuf0iok") then
+		 hiddenfling = true
+	 else
+		 hiddenfling = true
+		 detection = Instance.new("Decal")
+		 detection.Name = "juisdfj0i32i0eidsuf0iok"
+		 detection.Parent = game:GetService("ReplicatedStorage")
+		 local function fling()
+			 local hrp, c, vel, movel = nil, nil, nil, 0.1
+			 while true do
+				 game:GetService("RunService").Heartbeat:Wait()
+				 if hiddenfling then
+					 local lp = game.Players.LocalPlayer
+					 while hiddenfling and not (c and c.Parent and hrp and hrp.Parent) do
+						 game:GetService("RunService").Heartbeat:Wait()
+						 c = lp.Character
+						 hrp = c:FindFirstChild("HumanoidRootPart") or c:FindFirstChild("Torso") or c:FindFirstChild("UpperTorso")
+					 end
+					 if hiddenfling then
+						 vel = hrp.Velocity
+						 hrp.Velocity = vel * 10000 + Vector3.new(0, 10000, 0)
+						 game:GetService("RunService").RenderStepped:Wait()
+						 if c and c.Parent and hrp and hrp.Parent then
+							 hrp.Velocity = vel
+						 end
+						 game:GetService("RunService").Stepped:Wait()
+						 if c and c.Parent and hrp and hrp.Parent then
+							 hrp.Velocity = vel + Vector3.new(0, movel, 0)
+							 movel = movel * -1
+						 end
+					 end
+				 end
+			 end
+		 end
+		 
+		 fling()
 end
-end
-
-game:GetService("StarterGui"):SetCore("SendNotification", { 
-	Title = "Notification";
-	Text = "Netless activated";
-	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
-Duration = 16;
