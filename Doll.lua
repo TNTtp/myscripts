0bd40ff984f40046d563787fddc33a6b46d76f23
TNTtp <99587615+TNTtp@users.noreply.github.com>
2023-09-03 22:31:41 +0200

    Update Doll.lua

--- Doll.lua
+++ Doll.lua
@@ -1,5 +1,7 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
+hiddenfling = false
+
 local Window = OrionLib:MakeWindow({
 	Name = "BlcHUB",
 	HidePremium = false,
@@ -7,15 +9,62 @@ local Window = OrionLib:MakeWindow({
 	ConfigFolder = "BlcHUB"
 })
 
-local Tab = Window:MakeTab({
-	Name = "Fling",
+local Scripts = Window:MakeTab({
+	Name = "Scripts",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 
+local Fling = Tab:AddSection({
+	Name = "Fling"
+})
+
+Fling:AddToggle({
+	Name = "TouchFling",
+	Default = false,
+	Callback = function(Value)
+		if game:GetService("ReplicatedStorage"):FindFirstChild("juisdfj0i32i0eidsuf0iok") then
+	hiddenfling = Value
+else
+	hiddenfling = Value
+	detection = Instance.new("Decal")
+	detection.Name = "juisdfj0i32i0eidsuf0iok"
+	detection.Parent = game:GetService("ReplicatedStorage")
+	local function fling()
+		local hrp, c, vel, movel = nil, nil, nil, 0.1
+		while true do
+			game:GetService("RunService").Heartbeat:Wait()
+			if hiddenfling then
+				local lp = game.Players.LocalPlayer
+				while hiddenfling and not (c and c.Parent and hrp and hrp.Parent) do
+					game:GetService("RunService").Heartbeat:Wait()
+					c = lp.Character
+					hrp = c:FindFirstChild("HumanoidRootPart") or c:FindFirstChild("Torso") or c:FindFirstChild("UpperTorso")
+				end
+				if hiddenfling then
+					vel = hrp.Velocity
+					hrp.Velocity = vel * 1000000 + Vector3.new(0, 1000000, 0)
+					game:GetService("RunService").RenderStepped:Wait()
+					if c and c.Parent and hrp and hrp.Parent then
+							hrp.Velocity = vel
+					end
+					game:GetService("RunService").Stepped:Wait()
+					if c and c.Parent and hrp and hrp.Parent then
+						hrp.Velocity = vel + Vector3.new(0, movel, 0)
+						movel = movel * -1
+					end
+				end
+			end
+		end
+	end
+		 
+	fling()
+			end
+	end    
+})
 
 
-
+--[[
 game.Players.LocalPlayer.Character.HumanoidRootPart.Transparency = 0.5
 hiddenfling = false
 
