bf66fb36ca8cb2dcbceb41d3d3d9d42c83f18cb2
TNTtp <99587615+TNTtp@users.noreply.github.com>
2023-09-03 22:15:54 +0200

    Update Doll.lua

--- Doll.lua
+++ Doll.lua
@@ -1,11 +1,11 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 local Window = OrionLib:MakeWindow({
-		Name = "Title of the library",
-		HidePremium = false,
-		SaveConfig = true,
-		ConfigFolder = "BlcHUB"
-	})
+	Name = "Title of the library",
+	HidePremium = false,
+	SaveConfig = true,
+	ConfigFolder = "BlcHUB"
+})
 
 
 
