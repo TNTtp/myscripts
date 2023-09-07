bba852a1535a19aefe0206c19d50f979a7ac209d
TNTtp <99587615+TNTtp@users.noreply.github.com>
2023-09-07 23:50:42 +0200

    Update BlockHUB.lua

--- BlockHUB.lua
+++ BlockHUB.lua
@@ -354,7 +354,6 @@ Scripts:AddButton({
 	   DONE = true
 	   local qot = print("a")
 	   local hrp = game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-	   local link = "https://raw.githubusercontent.com/tnttp/myscripts/main/BlockHUB.lua"
 	   if hrp then
 		 qot = 'task.spawn(function() end) repeat wait() until game and game:IsLoaded() local lp = game:GetService('Players').LocalPlayer local char = lp.Character or lp.CharacterAdded:Wait() repeat char:WaitForChild('HumanoidRootPart').CFrame = CFrame.new("..tostring(hrp.CFrame)..") wait() until (Vector3.new("..tostring(hrp.Position)..") - char:WaitForChild('HumanoidRootPart').Position).Magnitude < 10 loadstring(game:HttpGet("https://raw.githubusercontent.com/tnttp/myscripts/main/BlockHUB.lua"))()'
 	   end
