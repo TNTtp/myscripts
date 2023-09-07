fc92a1629fae6da779ed6ab3cb41a2a87210a7c2
TNTtp <99587615+TNTtp@users.noreply.github.com>
2023-09-07 23:29:29 +0200

    Update BlockHUB.lua

--- BlockHUB.lua
+++ BlockHUB.lua
@@ -355,7 +355,7 @@ Scripts:AddButton({
 	   local qot = print("a")
 	   local hrp = game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
 	   if hrp then
-		 qot = "task.spawn(function() end) repeat wait() until game and game:IsLoaded() local lp = game:GetService('Players').LocalPlayer local char = lp.Character or lp.CharacterAdded:Wait() repeat char:WaitForChild('HumanoidRootPart').CFrame = CFrame.new("..tostring(hrp.CFrame)..") wait() until (Vector3.new("..tostring(hrp.Position)..") - char:WaitForChild('HumanoidRootPart').Position).Magnitude < 10 loadstring(game:HttpGet(/"https://raw.githubusercontent.com/tnttp/myscripts/main/BlockHUB.lua"/))()"
+		 qot = "task.spawn(function() end) repeat wait() until game and game:IsLoaded() local lp = game:GetService('Players').LocalPlayer local char = lp.Character or lp.CharacterAdded:Wait() repeat char:WaitForChild('HumanoidRootPart').CFrame = CFrame.new("..tostring(hrp.CFrame)..") wait() until (Vector3.new("..tostring(hrp.Position)..") - char:WaitForChild('HumanoidRootPart').Position).Magnitude < 10 loadstring(game:HttpGet(..'"'.."https://raw.githubusercontent.com/tnttp/myscripts/main/BlockHUB.lua"..'"'..))()"
 	   end
 	   queueteleport(qot)
 	   game:GetService("TeleportService"):TeleportCancel()
