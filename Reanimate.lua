1236dc33bb8f09eb3d3c4fd4c2fda76448d9f14d
TNTtp <99587615+TNTtp@users.noreply.github.com>
2023-09-12 19:58:10 +0200

    Update Reanimate.lua

--- Reanimate.lua
+++ Reanimate.lua
@@ -1,1 +1,52 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({
+	Name = "BlockHUB📦 v1",
+	HidePremium = false,
+	SaveConfig = true,
+	ConfigFolder = "BlockHUB"
+})
+
+local Scripts = Window:MakeTab({
+	Name = "Scripts",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local choose = Scripts:AddDropdown({
+	Name = "Select player",
+	Default = "None",
+	Options = {"None",
+	"Astronaut",
+	"Bubbly",
+	"Confident",
+	"Cartoony",
+	"Cowboy",
+	"Elder",
+	"Knight",
+	"Levitation",
+	"Mage",
+	"Ninja",
+	"Oldschool",
+	"Popstar",
+	"Pirate",
+	"Patrol",
+	"Princess",
+	"Robot",
+	"R15",
+	"Stylish",
+	"Sneaky",
+	"Superhero",
+	"Toy",
+	"Vampire",
+	"Werewolf",
+	"Zombie",
+    },
+	Flag = "select",
+	Callback = function(Value)
+		
+	end    
+})
+
+
 
