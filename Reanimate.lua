e725ee7d4686f3b9d57e493ad72ceef9638f3ea6
TNTtp <99587615+TNTtp@users.noreply.github.com>
2023-09-12 19:55:19 +0200

    Update Reanimate.lua

--- Reanimate.lua
+++ Reanimate.lua
@@ -1,10 +1,1 @@
-local camera = game.Workspace.CurrentCamera
-local humanoid = game.Players.Localplayer
 
-if (humanoid.MoveDirection:Dot(camera.CFrame.RightVector) > 0.75) then
-	-- we're going right
-end
-
-if (boostPad.CFrame.LookVector:Dot(humanoid.MoveDirection) > 0.75) then
-	-- boost!
-end
