23d0b6260cd8766ab9e4b16caccb12354730bdfc
TNTtp <99587615+TNTtp@users.noreply.github.com>
2023-09-14 11:09:43 +0200

    Update Glitch.lua

--- Glitch.lua
+++ Glitch.lua
@@ -1,1 +1,12 @@
 
+local Anim = Instance.new("Animation")
+Anim.AnimationId = "rbxassetid://184574340"
+local track = game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(Anim)
+
+while wait() do
+         if track.IsPlaying == false then
+            
+                track:Play(.1, 1, 40)
+            
+         end
+end
